477168124e42f052f3310d70fb0121428d886a4b
Olha36 <olhakucheruk36@gmail.com>
2025-01-17 12:57:19 +0200

    add login component

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,29 +1,35 @@
-import React from 'react'
-import { Alert, Button, Form, Row, Col, Stack} from "react-bootstrap";
+import React from "react";
+import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
 
 const Register = () => {
   return (
     <>
       <Form>
-        <Row style={{
-          height: "100vh",
-          justifyContent: "center",
-          paddingTop: "10%"
-        }}>
+        <Row
+          style={{
+            height: "100vh",
+            justifyContent: "center",
+            paddingTop: "10%",
+          }}
+        >
           <Col xs={6}>
-          <Stack gap={3}>
-<h2>Register</h2>
-<Form.Control type='text' placeholder='name'/>
-<Form.Control type='email' placeholder='Email'/>
-<Form.Control type='password' placeholder='Password'/>
-<Button variant='primary' type="submit">Register</Button>
-<Alert variant='danger'><p>An error occured</p></Alert>
-          </Stack>
+            <Stack gap={3}>
+              <h2>Register</h2>
+              <Form.Control type="text" placeholder="name" />
+              <Form.Control type="email" placeholder="Email" />
+              <Form.Control type="password" placeholder="Password" />
+              <Button variant="primary" type="submit">
+                Register
+              </Button>
+              <Alert variant="danger">
+                <p>An error occured</p>
+              </Alert>
+            </Stack>
           </Col>
         </Row>
       </Form>
     </>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
