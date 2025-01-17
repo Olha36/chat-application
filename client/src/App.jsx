9a123093239622f07327389170c7cf836df298bc
Olha36 <olhakucheruk36@gmail.com>
2025-01-17 12:29:12 +0200

    add NavBar component, replace index css and App.jsx in main.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,8 @@ import {Routes, Route, Navigate} from 'react-router-dom';
 import Chat from './pages/Chat';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import "bootstrap/dist/css/bootstrap.min.css";
-import {Container} from 'react-bootstrap';
+import  "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from 'react-bootstrap';
 import NavBar from "./components/NavBar"
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
     <NavBar/>
-    <Container >
+    <Container>
       <Routes>
           <Route path='/' element={ <Chat /> } />
           <Route path='/register' element={<Register />} />
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -6,10 +6,16 @@ const NavBar = () => {
   return (
     <Navbar bg="dark" className='mb-4' style={{height: '3.75rem'}}>
         <Container>
-            <Link to='/'>
-            <h2>Chatapp</h2>
-            </Link>
-            
+            <h2>
+            <Link to='/' className='link-light text-decoration-none'>ChattApp </Link>
+            </h2>
+
+            <Nav>
+                <Stack direction='horizontal' gap={3}>
+                <Link to='/login' className='link-light text-decoration-none'>Login </Link>
+                <Link to='/register' className='link-light text-decoration-none'>Register </Link>
+                </Stack>
+            </Nav>
         </Container>
     </Navbar>
   )
