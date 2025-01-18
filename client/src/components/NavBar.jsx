2337b16034130218c03ad2614970f8d0bf1de3c1
Olha36 <olhakucheruk36@gmail.com>
2025-01-18 18:56:30 +0200

    Add logout function and condition to display user name in NavBar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 
 const NavBar = () => {
-  const { user } = useContext(AuthContext);
+  const { user, logoutUser } = useContext(AuthContext);
   return (
     <Navbar bg="dark" className="mb-4" style={{ height: "3.75rem" }}>
       <Container>
@@ -14,15 +14,34 @@ const NavBar = () => {
             ChattApp{" "}
           </Link>
         </h2>
-        <span className="text-warning">Logged in as {user?.name} </span>
+        {user && <span className="text-warning">Logged in as {user?.name} </span> }
         <Nav>
           <Stack direction="horizontal" gap={3}>
-            <Link to="/login" className="link-light text-decoration-none">
-              Login{" "}
-            </Link>
-            <Link to="/register" className="link-light text-decoration-none">
-              Register{" "}
-            </Link>
+            {user && (
+              <>
+                <Link
+                  onClick={() => logoutUser()}
+                  to="/login"
+                  className="link-light text-decoration-none"
+                >
+                  Logout
+                </Link>
+              </>
+            )}
+
+            {!user && (
+              <>
+                <Link to="/login" className="link-light text-decoration-none">
+                  Login
+                </Link>
+                <Link
+                  to="/register"
+                  className="link-light text-decoration-none"
+                >
+                  Register
+                </Link>
+              </>
+            )}
           </Stack>
         </Nav>
       </Container>
