423d07047d37e5fec2f956fd23e3983589c8f9b5
Olha36 <olhakucheruk36@gmail.com>
2025-01-18 19:48:11 +0200

    add a createChat function in Controller.js for creating a chat

---
+++ server/Controllers/chatController.js
@@ -0,0 +1,27 @@
+const chatModel = require("../Models/chatModel");
+// createChat
+
+const createChat = async (req, res) => {
+  const { firstId, secondId } = req.body;
+
+  try {
+    const chat = await chatModel.findOne({
+      members: { $all: [firstId, secondId] },
+    });
+
+    if (chat) return res.status(200).json(chat);
+
+    const newChat = new chatModel({
+      members: [firstId, secondId],
+    });
+
+    const response = await newChat.save();
+
+    res.status(200).json(response);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+// getAllChats
+// find chat
