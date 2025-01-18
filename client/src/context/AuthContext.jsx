bc0cf4e60c3fdf0224170a986b786185024ef685
Olha36 <olhakucheruk36@gmail.com>
2025-01-18 13:36:33 +0200

    add registerUser function for registering users

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,10 +1,12 @@
 import { createContext, useCallback, useState } from "react";
+import { baseUrl, postRequest } from "../utils/services";
 
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-
+  const [registerError, setRegisterError] = useState(null);
+  const [isRegisterLoading, setisRegisterLoading] = useState(false);
   const [registerInfo, setRegisterInfo] = useState({
     name: "",
     email: "",
@@ -12,9 +14,28 @@ export const AuthContextProvider = ({ children }) => {
   });
 
   console.log("register info is:", registerInfo);
-  
+
   const updateRegisterInfo = useCallback((info) => {
     setRegisterInfo(info);
+  }, []);
+
+  const registerUser = useCallback(async () => {
+    setisRegisterLoading(true);
+    setRegisterError(null);
+
+    const response = await postRequest(
+      `${baseUrl}/users/register`,
+      JSON.stringify(registerInfo)
+    );
+
+    setisRegisterLoading(false);
+
+    if (response.error) {
+      return setRegisterError(response);
+    }
+
+    localStorage.setItem("User", JSON.stringify(response));
+    setUser(response);
   }, []);
 
   return (
