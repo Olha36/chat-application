70b9f1c70c3eb41eec33678b98a936d5252cb467
Olha36 <olhakucheruk36@gmail.com>
2025-01-18 12:49:02 +0200

    Add onChange handler for updating registerInfo state

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,12 +1,31 @@
-import { createContext, useState } from "react";
+import { createContext, useCallback, useState } from "react";
 
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
-  const [user, setUser] = useState({
-    name: "Olha",
+  const [user, setUser] = useState(null);
+
+  const [registerInfo, setRegisterInfo] = useState({
+    name: "",
+    email: "",
+    password: "",
   });
+
+  console.log("register info is:", registerInfo);
+  
+  const updateRegisterInfo = useCallback((info) => {
+    setRegisterInfo(info);
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider
+      value={{
+        user,
+        registerInfo,
+        updateRegisterInfo,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
   );
 };
