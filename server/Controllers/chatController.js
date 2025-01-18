0c1e972949438fccece145644b0c45558b8ea68e
Olha36 <olhakucheruk36@gmail.com>
2025-01-18 19:55:58 +0200

    create a function to find a chat

--- server/Controllers/chatController.js
+++ server/Controllers/chatController.js
@@ -34,10 +34,24 @@ const findUserChats = async (req, res) => {
     });
 
     res.status(200).json(chats);
-    
   } catch (error) {
     console.log(error);
     res.status(500).json(error);
   }
 };
+
 // find chat
+const findChat = async (req, res) => {
+  const { firstId, secondId } = req.params;
+
+  try {
+    const chat = await chatModel.find({
+      members: { $all: [firstId, secondId] },
+    });
+
+    res.status(200).json(chat);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
