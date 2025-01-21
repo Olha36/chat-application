b8e570f54f7dfecc03f1d712b83679cc61a5dfbf
Olha36 <olhakucheruk36@gmail.com>
2025-01-21 13:40:38 +0200

    Add UI elements for displaying user chat details in UserChat component

--- client/src/components/chat/UserChat.jsx
+++ client/src/components/chat/UserChat.jsx
@@ -1,11 +1,30 @@
+import { Stack } from "react-bootstrap";
 import { useFetchRecipientUser } from "../../hooks/useFetchRecipient";
 
 const UserChat = ({ chat, user }) => {
   const { recipientUser } = useFetchRecipientUser(chat, user);
 
-  console.log("recipient user is:", recipientUser);
+  return (
+    <Stack
+      direction="horizontal"
+      gap={3}
+      className="user-card align-items-center p-2 justify-content-between"
+    >
+      <div className="d-flex">
+        <div className="me-2">A</div>
+        <div className="text-content">
+          <div className="name">{recipientUser?.name}</div>
+          <div className="text">Message text</div>
+        </div>
+      </div>
 
-  return <>User chat</>;
+      <div className="d-flex flex-column align-items-end">
+        <div className="date">21.01.2025</div>
+        <div className="this-user-notifications">2</div>
+        <div className="user-online"></div>
+      </div>
+    </Stack>
+  );
 };
 
 export default UserChat;
