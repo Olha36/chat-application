6bc4f7eee7409520f16139bfed585eb1db66c180
Olha36 <olhakucheruk36@gmail.com>
2025-01-22 16:13:11 +0200

    install moment + add loading state and message display to ChatBox component

--- client/src/components/chat/ChatBox.jsx
+++ client/src/components/chat/ChatBox.jsx
@@ -1,7 +1,9 @@
 import { useContext } from "react";
+import { Stack } from "react-bootstrap";
 import { AuthContext } from "../../context/authContext";
 import { ChatContext } from "../../context/chatContext";
 import { useFetchRecipientUser } from "../../hooks/useFetchRecipient";
+import moment from "moment";
 
 const ChatBox = () => {
   const { user } = useContext(AuthContext);
@@ -17,7 +19,37 @@ const ChatBox = () => {
       </p>
     );
 
-  return <>ChatBox</>;
+  if (isMessagesLoading) {
+    return (
+      <p style={{ textAlign: "center", width: "100%" }}>Loading chat...</p>
+    );
+  }
+
+  return (
+    <Stack gap={4} className="chat-box">
+      <div className="chat-header">
+        <strong>{recipientUser?.name}</strong>
+      </div>
+      <Stack gap={3} className="messages">
+        {messages &&
+          messages.map((message, index) => (
+            <Stack
+              key={index}
+              className={`${
+                message?.senderId === user?._id
+                  ? "message self align-self-end flex-grow-0"
+                  : "message align-self-start flex-grow-0"
+              }`}
+            >
+              <span>{message.text}</span>
+              <span className="message-footer">
+                {moment(message.createdAt).calendar()}
+              </span>
+            </Stack>
+          ))}
+      </Stack>
+    </Stack>
+  );
 };
 
 export default ChatBox;
