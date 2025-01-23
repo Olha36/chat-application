066488d4b6be62e24604ecc9af01108c7daaf409
Olha36 <olhakucheruk36@gmail.com>
2025-01-23 17:32:51 +0200

    implement logic for showing short text message and date

--- client/src/components/chat/UserChat.jsx
+++ client/src/components/chat/UserChat.jsx
@@ -4,11 +4,15 @@ import avatar from "../../assets/avatar.svg";
 import { useContext } from "react";
 import { ChatContext } from "../../context/chatContext";
 import { unreadNotificationsFunc } from "../../utils/unreadNotifications";
+import { useFetchLatestMessage } from "../../hooks/useFetchLatestMessage";
+import moment from "moment";
 
 const UserChat = ({ chat, user }) => {
   const { recipientUser } = useFetchRecipientUser(chat, user);
   const { onlineUsers, notifications, markThisUserNotificationsAsRead } =
     useContext(ChatContext);
+
+  const { latestMessage } = useFetchLatestMessage(chat);
 
   const unreadNotifications = unreadNotificationsFunc(notifications);
   const thisUserNotifications = unreadNotifications?.filter(
@@ -18,6 +22,16 @@ const UserChat = ({ chat, user }) => {
   const isOnline = onlineUsers?.some(
     (user) => user?.userId === recipientUser?._id
   );
+
+  const truncateText = (text) => {
+    let shortText = text.substring(0, 20);
+
+    if (text.length > 20) {
+      shortText = shortText + "...";
+    }
+
+    return shortText;
+  };
 
   return (
     <Stack
@@ -37,12 +51,18 @@ const UserChat = ({ chat, user }) => {
         </div>
         <div className="text-content">
           <div className="name">{recipientUser?.name}</div>
-          <div className="text">Message text</div>
+          <div className="text">
+            {latestMessage?.text && (
+              <span>{truncateText(latestMessage?.text)}</span>
+            )}
+          </div>
         </div>
       </div>
 
       <div className="d-flex flex-column align-items-end">
-        <div className="date">21.01.2025</div>
+        <div className="date">
+          {moment(latestMessage?.createdAt).calendar()}
+        </div>
         <div
           className={
             thisUserNotifications?.length > 0 ? "this-user-notifications" : ""
