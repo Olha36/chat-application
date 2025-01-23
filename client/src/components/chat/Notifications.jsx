89a220009d7080dd2214d919d70f78c4f8323d53
Olha36 <olhakucheruk36@gmail.com>
2025-01-23 16:31:53 +0200

    add unread notifications count UI

--- client/src/components/chat/Notifications.jsx
+++ client/src/components/chat/Notifications.jsx
@@ -10,16 +10,16 @@ const Notifications = () => {
 
   const unreadNofitications = unreadNotificationsFunc(notifications);
   const modifiedNotifications = notifications.map((n) => {
-    const sender = allUsers.find(user => user._id === n.senderId);
+    const sender = allUsers.find((user) => user._id === n.senderId);
 
     return {
       ...n,
-      senderName: sender?.name
-    }
+      senderName: sender?.name,
+    };
   });
 
-  console.log('unread not:', unreadNofitications);
-  console.log('modified not:', modifiedNotifications);
+  console.log("unread not:", unreadNofitications);
+  console.log("modified not:", modifiedNotifications);
 
   return (
     <div className="notifications">
@@ -35,6 +35,11 @@ const Notifications = () => {
           <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
           <path d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5M3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6m0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5" />
         </svg>
+        {unreadNofitications?.length === 0 ? null : (
+          <span className="notification-count">
+            <span>{unreadNofitications?.length}</span>
+          </span>
+        )}
       </div>
       {isOpen ? (
         <div className="notifications-box">
