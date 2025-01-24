a6ab4ea20b996684b1327fadf9436074391fa78f
Olha36 <olhakucheruk36@gmail.com>
2025-01-24 16:58:26 +0200

    add media queries for the responsive layout

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -15,7 +15,7 @@ const Chat = () => {
     <Container>
       <PotentialChats />
       {userChats?.length < 1 ? null : (
-        <Stack direction="horizontal" gap={4} className="align-items-start">
+        <Stack direction="horizontal" gap={4} className="align-items-start messages-container">
           <Stack className="messages-box flex-grow-0 pe-3" gap={3}>
             {isUserChatsLoading && <p>Loading chats...</p>}
             {userChats?.map((chat, index) => {
