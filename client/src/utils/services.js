523c2b82b5ddd3694df63743bbbcc4519902e14e
Olha36 <olhakucheruk36@gmail.com>
2025-01-29 15:50:18 +0200

    prepare for vercel deployment

--- client/src/utils/services.js
+++ client/src/utils/services.js
@@ -1,4 +1,4 @@
-export const baseUrl = "http://localhost:3000/api";
+export const baseUrl = `${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}/api`;
 
 export const postRequest = async (url, body) => {
   const response = await fetch(url, {
