a1a0a4925148819695782488932793b213bca48c
Olha36 <olhakucheruk36@gmail.com>
2025-01-31 12:00:11 +0200

    change HashRouter into BrowserRouter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <AuthContextProvider>
         <App />
       </AuthContextProvider>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
